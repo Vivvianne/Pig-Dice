9bf7c98e014bb85145988d65c63c85ea60283252
Vivvianne Kimani <ndiahav@gmail.com>
2019-05-10 14:17:08 +0300

    wrote the prototype function for roll1 with if and else

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,11 @@ function Roller(turn){
     this.turn = turn;
     this.rollerName;
 }
+roller.prototype.roll1 = function(){
+    if (this.roll ===1) {
+        this.tempscore=0;
+        alert("Very sorry" + this.rollerName + " ,you got a 1, your turn is over")
+        }else {
+            this.tempscore += this.roll;
+    }
+}
