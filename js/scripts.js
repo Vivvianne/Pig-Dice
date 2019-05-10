d74148f3ab3434c67fcdc8e400413a00d6e875dd
Vivvianne Kimani <ndiahav@gmail.com>
2019-05-10 14:08:10 +0300

    wrote the function for roller turn

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ var roller2 = "";
 var throwdice = function(){
     return Math.floor(6* Math.random()) +1
 }
+function Roller(turn){
+    this.roll = 0;
+    this.tempscore = 0;
+    this.totalscore = 0;
+    this.turn = turn;
+    this.rollerName;
+}
